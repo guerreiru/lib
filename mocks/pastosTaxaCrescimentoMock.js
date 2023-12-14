93b32749d37d57999ff03c5b6cf92e8720e98599
Guerreiro <fernando.rodrigues@prodap.com.br>
2023-12-14 20:11:00 -0300

    feat: adicionando novos mocks

---
+++ mocks/pastosTaxaCrescimentoMock.js
@@ -0,0 +1,11 @@
+const pastosTaxaCrescimentoMock = [
+  {
+    IdPasto: 181,
+    IdRetiro: 31,
+    IdFazenda: 16,
+    IdForragem: 1,
+    IdModulo: 406,
+    OrdemModulo: 1,
+  },
+];
+module.exports = { pastosTaxaCrescimentoMock };
