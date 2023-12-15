2885af55230346b1902e4d1fb2e77d9c21bc5c7d
Guerreiro <fernando.rodrigues@prodap.com.br>
2023-12-14 21:04:46 -0300

    feat: função monthName para retornar o nome do mes

---
+++ helpers/monthName.js
@@ -0,0 +1,20 @@
+/**
+ * Returns the name of the month in Portuguese for the given month index.
+ *
+ * @param {number} monthIndex - The month index (0-11).
+ * @returns {string} The name of the month in lowercase.
+ */
+
+const monthName = (currentMonth) => {
+  const monthIndex = currentMonth ?? new Date().getMonth();
+
+  const formatter = new Intl.DateTimeFormat("pt-BR", {
+    month: "long",
+  });
+
+  return formatter.format(new Date(0, monthIndex));
+};
+
+const monthNames = () => Array.from({ length: 12 }, (_, idx) => monthName(idx));
+
+module.exports = { monthName, monthNames };
