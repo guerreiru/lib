e632b3390e66c310f84f6db0676843235c3f689e
Fernando <f.luhis@gmail.com>
2023-12-28 19:32:55 -0300

    fix: alterando DateManipulator para classe e adicionando JSDoc

--- helpers/date.js
+++ helpers/date.js
@@ -1,30 +1,71 @@
-const DateManipulator = {
-  currentDate: () => new Date(),
+/**
+ * Classe para manipulação de datas.
+ */
+class DateManipulator {
+  /**
+   * Obtém a data e hora atual.
+   * @returns {Date} A data e hora atual.
+   */
+  static currentDate() {
+    return new Date();
+  }
 
-  addHours: (amountOfHours, date = new Date()) => {
+  /**
+   * Adiciona uma quantidade específica de horas a uma data.
+   * @param {number} amountOfHours - A quantidade de horas a serem adicionadas.
+   * @param {Date} [date=currentDate()] - A data à qual adicionar as horas.
+   * @returns {Date} Uma nova instância de data após a adição de horas.
+   * @throws {Error} Se a quantidade de horas não for um número.
+   */
+  static addHours(amountOfHours, date = this.currentDate()) {
+    if (typeof amountOfHours !== "number") {
+      throw new Error("A quantidade de horas deve ser um número.");
+    }
+
     const newDate = new Date(date);
     newDate.setHours(newDate.getHours() + amountOfHours);
     return newDate;
-  },
+  }
 
-  subtractHours: (amountOfHours, date = new Date()) => {
+  /**
+   * Subtrai uma quantidade específica de horas de uma data.
+   * @param {number} amountOfHours - A quantidade de horas a serem subtraídas.
+   * @param {Date} [date=currentDate()] - A data à qual subtrair as horas.
+   * @returns {Date} Uma nova instância de data após a subtração de horas.
+   * @throws {Error} Se a quantidade de horas não for um número.
+   */
+  static subtractHours(amountOfHours, date = this.currentDate()) {
+    if (typeof amountOfHours !== "number") {
+      throw new Error("A quantidade de horas deve ser um número.");
+    }
+
     const newDate = new Date(date);
     newDate.setHours(newDate.getHours() - amountOfHours);
     return newDate;
-  },
+  }
 
-  getStartOfDay: (date) => {
+  /**
+   * Obtém o início do dia para uma determinada data.
+   * @param {Date} [date=currentDate()] - A data para a qual obter o início do dia.
+   * @returns {Date} Uma nova instância de data representando o início do dia.
+   */
+  static getStartOfDay(date = this.currentDate()) {
     const newDate = new Date(date);
     newDate.setHours(0, 0, 0, 0);
     return newDate;
-  },
+  }
 
-  getEndOfDay: (date) => {
+  /**
+   * Obtém o final do dia para uma determinada data.
+   * @param {Date} [date=currentDate()] - A data para a qual obter o final do dia.
+   * @returns {Date} Uma nova instância de data representando o final do dia.
+   */
+  static getEndOfDay(date = this.currentDate()) {
     const newDate = new Date(date);
     newDate.setHours(23, 59, 59, 999);
     return newDate;
-  },
-};
+  }
+}
 
 module.exports = {
   DateManipulator,
