1010155b86c925f2e57311881e78a14345c3f34a
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2024-06-04 17:20:49 -0300

    adicionando documentação

--- helpers/date.js
+++ helpers/date.js
@@ -79,6 +79,24 @@ class DateManipulator {
   static diff(startDate, endDate) {
     return dateDiff(startDate, endDate);
   }
+
+  /**
+   * Retorna a data correspondente ao número especificado de dias atrás a partir da data atual.
+   * Se nenhum número de dias for fornecido, retorna a data de um dia atrás.
+   * @param {number} days - O número de dias atrás a partir da data atual.
+   * @returns {Date} - A data correspondente ao número especificado de dias atrás.
+   */
+  static daysAgo = (days) => {
+    if (!days) {
+      return this.daysAgo(1);
+    }
+
+    const currDate = new Date();
+    const subtractedDate = new Date(
+      currDate.setDate(currDate.getDate() - days)
+    );
+    return subtractedDate;
+  };
 }
 
 module.exports = {
--- helpers/daysAgo.js
+++ helpers/daysAgo.js
@@ -1,6 +1,12 @@
+/**
+ * Retorna a data correspondente ao número especificado de dias atrás a partir da data atual.
+ * Se nenhum número de dias for fornecido, retorna a data de um dia atrás.
+ * @param {number} days - O número de dias atrás a partir da data atual.
+ * @returns {Date} - A data correspondente ao número especificado de dias atrás.
+ */
 const daysAgo = (days) => {
   if (!days) {
-    return new Date();
+    return daysAgo(1);
   }
 
   const currDate = new Date();
